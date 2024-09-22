68035a5cf34067bade77e9105a328da473abeffb
ali_maameri02 <ali.maameri@univ-constanine2.com>
2024-09-22 01:06:39 +0100

    docker + nginx files

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,24 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // base: '/Propelo_front.git/',
-
+  base: '/',  // Sets the base path for the production build.
   plugins: [react()],
-//   server: {
-//         proxy: {
-//             '/api': {
-//                 target: 'http://propelo.runasp.net',
-//                 changeOrigin: true,
-//                 secure: false,
-//             }
-//         }
-//     }
-
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://propelo.runasp.net',  // Pointing to the backend server for API calls
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',  // Specify the output directory for production build
+    sourcemap: false, // Optional: Disable sourcemaps for smaller production files
+  },
+  resolve: {
+    alias: {
+      '@': '/src',  // Optional: Aliases if you use absolute imports
+    }
+  },
 })
